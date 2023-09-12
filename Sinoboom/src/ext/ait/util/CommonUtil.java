6ffa49d3464a93829d24ed2ca190e4aa22f81f54
dzzzzz123 <3109864016@qq.com>
2023-09-12 21:34:34 +0800

    更新util

--- Sinoboom/src/ext/ait/util/CommonUtil.java
+++ Sinoboom/src/ext/ait/util/CommonUtil.java
@@ -1,7 +1,5 @@
 package ext.ait.util;
 
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Locale;
@@ -424,14 +422,4 @@ public class CommonUtil {
 		return map;
 	}
 
-	/**
-	 * 连接Windchill数据库来运行自定义sql
-	 * @return WTConnection 数据库连接
-	 * @throws Exception
-	 */
-	public static WTConnection getWTConnection() throws Exception {
-		MethodContext methodcontext = MethodContext.getContext();
-		WTConnection wtconnection = (WTConnection) methodcontext.getConnection();
-		return wtconnection;
-	}
 }
